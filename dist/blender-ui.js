5f7b3b541aace45d5883b4e9c645de494d955158
tuok1997 <badalov.turxan@gmail.com>
2018-04-18 03:29:57 +0400

    Added css files and images

--- dist/blender-ui.js
+++ dist/blender-ui.js
@@ -10,6 +10,55 @@
 		return module = { exports: {} }, fn(module, module.exports), module.exports;
 	}
 
+	var findindexPolyfill = function findindexPolyfill() {
+	          // https://tc39.github.io/ecma262/#sec-array.prototype.findIndex
+	          if (!Array.prototype.findIndex) {
+	                    Object.defineProperty(Array.prototype, 'findIndex', {
+	                              value: function value(predicate) {
+	                                        // 1. Let O be ? ToObject(this value).
+	                                        if (this == null) {
+	                                                  throw new TypeError('"this" is null or not defined');
+	                                        }
+
+	                                        var o = Object(this);
+
+	                                        // 2. Let len be ? ToLength(? Get(O, "length")).
+	                                        var len = o.length >>> 0;
+
+	                                        // 3. If IsCallable(predicate) is false, throw a TypeError exception.
+	                                        if (typeof predicate !== 'function') {
+	                                                  throw new TypeError('predicate must be a function');
+	                                        }
+
+	                                        // 4. If thisArg was supplied, let T be thisArg; else let T be undefined.
+	                                        var thisArg = arguments[1];
+
+	                                        // 5. Let k be 0.
+	                                        var k = 0;
+
+	                                        // 6. Repeat, while k < len
+	                                        while (k < len) {
+	                                                  // a. Let Pk be ! ToString(k).
+	                                                  // b. Let kValue be ? Get(O, Pk).
+	                                                  // c. Let testResult be ToBoolean(? Call(predicate, T, « kValue, k, O »)).
+	                                                  // d. If testResult is true, return k.
+	                                                  var kValue = o[k];
+	                                                  if (predicate.call(thisArg, kValue, k, o)) {
+	                                                            return k;
+	                                                  }
+	                                                  // e. Increase k by 1.
+	                                                  k++;
+	                                        }
+
+	                                        // 7. Return -1.
+	                                        return -1;
+	                              },
+	                              configurable: true,
+	                              writable: true
+	                    });
+	          }
+	};
+
 	//Returns id of the parent node
 	var getOption = function getOption(opt, name, def) {
 	    var value = opt[name];
@@ -663,52 +712,7 @@
 	  return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj;
 	};
 
-	// https://tc39.github.io/ecma262/#sec-array.prototype.findIndex
-	if (!Array.prototype.findIndex) {
-	    Object.defineProperty(Array.prototype, 'findIndex', {
-	        value: function value(predicate) {
-	            // 1. Let O be ? ToObject(this value).
-	            if (this == null) {
-	                throw new TypeError('"this" is null or not defined');
-	            }
-
-	            var o = Object(this);
-
-	            // 2. Let len be ? ToLength(? Get(O, "length")).
-	            var len = o.length >>> 0;
-
-	            // 3. If IsCallable(predicate) is false, throw a TypeError exception.
-	            if (typeof predicate !== 'function') {
-	                throw new TypeError('predicate must be a function');
-	            }
-
-	            // 4. If thisArg was supplied, let T be thisArg; else let T be undefined.
-	            var thisArg = arguments[1];
-
-	            // 5. Let k be 0.
-	            var k = 0;
-
-	            // 6. Repeat, while k < len
-	            while (k < len) {
-	                // a. Let Pk be ! ToString(k).
-	                // b. Let kValue be ? Get(O, Pk).
-	                // c. Let testResult be ToBoolean(? Call(predicate, T, « kValue, k, O »)).
-	                // d. If testResult is true, return k.
-	                var kValue = o[k];
-	                if (predicate.call(thisArg, kValue, k, o)) {
-	                    return k;
-	                }
-	                // e. Increase k by 1.
-	                k++;
-	            }
-
-	            // 7. Return -1.
-	            return -1;
-	        },
-	        configurable: true,
-	        writable: true
-	    });
-	}
+	findindexPolyfill();
 
 	var VIEWPORT_CONTENT_CLASS = 'viewport-content';
 	var HANDLE_CLASS = 'split-drag';
--- rollup.config.js
+++ rollup.config.js
@@ -1,42 +1,52 @@
 // rollup.config.js
+const PRODUCTION = 'production';
+const isProd = process.env.NODE_ENV === PRODUCTION;
 const resolve = require('rollup-plugin-node-resolve');
 const commonjs = require('rollup-plugin-commonjs');
 const babel = require('rollup-plugin-babel');
 const uglify = require('rollup-plugin-uglify');
 const postcss = require('rollup-plugin-postcss');
+const cssnext = require('postcss-cssnext');
+const cssnano = require('cssnano');
 //const buble = require('rollup-plugin-buble');
+
+
+const postcssPlugins = [cssnext()];
+if (isProd) {
+    postcssPlugins.push(
+        cssnano({
+            preset: ['default', {
+                discardComments: {
+                    removeAll: true
+                },
+                normalizeWhitespace: false
+            }]
+        })
+    );
+}
+
+const postcssConfig = {
+    extract: true,
+    extensions: [ '.css' ],
+    plugins: postcssPlugins
+};
 
 const defaultConfig = {
     input: 'src/blender-ui.js',
     output: {
-        file: 'dist/blender-ui.js',
+        file: `dist/blender-ui${isProd ? '.min' : ''}.js`,
         format: 'umd',
         name: 'blender-ui'
     },
     plugins: [
+        postcss(postcssConfig),
         resolve(),
         commonjs(),
         babel({
             exclude: 'node_modules/**'
-        })
+        }),
+        isProd && uglify()
     ]
 };
 
-
-function config(filename = `dist/${defaultConfig.output.name}.js`, plugins = [], moduleName = defaultConfig.output.name) {
-    console.log(filename);
-    const editedConfig = Object.assign({}, defaultConfig);
-    editedConfig.output = Object.assign({}, defaultConfig.output, {
-        file: filename,
-        format: defaultConfig.output.format,
-        name: moduleName
-    });
-    editedConfig.plugins = [].concat(editedConfig.plugins, plugins);
-
-    return editedConfig;
-}
-
-export default [
-    config(),
-    config(`dist/${defaultConfig.output.name}.min.js`, [ uglify() ])
-]
+export default defaultConfig;
